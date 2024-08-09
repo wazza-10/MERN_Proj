1f5b6db1643c9bf706723cb4e8d7a4cbe52f6205
wazza-10 <aakashshoraan7@gmail.com>
2024-08-08 23:46:52 -0400

    Working_setup+Styling

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { configureStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose} from 'redux';
+import {thunk} from 'redux-thunk';
 import App from './App';
 
 import reducers from './reducers';
 
-const store = configureStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,10 +1,10 @@
 const reducers = (posts = [], action) =>{
     switch (action.type) {
         case 'FETCH_ALL':
-            return posts;
+            return action.payload;
         case 'CREATE':
-            return posts;
-        default:
+            return [...posts, action.payload];
+        default: 
             return posts;
     }
 }
