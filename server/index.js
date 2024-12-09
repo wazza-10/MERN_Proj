da3e4aced90fd6938a6f9240a7a6e9bf7a4c3ff8
wazza-10 <aakashshoraan7@gmail.com>
2024-12-08 21:48:06 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.get('/', (req,res) => {
+    res.send('Hello to Memories API');
+});
 
 // const CONNECTION_URL = 'mongodb+srv://java_master:java_master123@cluster0.dtg3u5g.mongodb.net/';
 const PORT = process.env.PORT || 5000
