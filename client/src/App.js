75d345b6e79d705dfca4aac5fcb5679c4b8b3270
wazza-10 <aakashshoraan7@gmail.com>
2024-12-17 00:46:29 -0500

    Setting_up_Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container,Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import memories from './images/memories.png';
 import useStyles from './styles';
+import NavBar from './components/NavBar/NavBar';
 const App = () => {
     const [currentId, setCurrentId] = useState(null);
     const classes = useStyles();
@@ -17,10 +17,7 @@ const App = () => {
     }, [currentId, dispatch]);
     return (
         <Container maxidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
-            </AppBar>
+            <NavBar />
             <Grow in>
                 
                 <Container>
