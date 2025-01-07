00c416a0f44b7388981605d9b29f0bb8cc5c5914
wazza-10 <aakashshoraan7@gmail.com>
2025-01-06 20:55:33 -0500

    Connecting-Frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API = axios.create( {baseURL: 'http://localhost:5000' } );
+const API = axios.create( {baseURL: 'https://insta-memories-app-23d210188f5b.herokuapp.com/posts' } );
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
